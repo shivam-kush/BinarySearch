e8d1522034a9f8312745e0fe3577d4c66f547167
2pieare <91305830+shivam-kush@users.noreply.github.com>
2022-01-31 00:00:14 +0530

    Create Q2 Sum of First N Odd Integers.cpp

---
+++ 30Jan/Q2 Sum of First N Odd Integers.cpp
@@ -0,0 +1,15 @@
+int solve(int n) {
+    /*int arr[1000];
+    arr[0] = 1;
+    for(int i=1; i<1000;i++){
+        arr[i] = arr[i-1] + 2;
+    }
+    int sum =0;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+    }
+    return sum;
+    */
+    int oddsum = n*n;
+    return oddsum;
+}
